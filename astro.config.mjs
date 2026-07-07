4d56c5f5421c70433e3aaf6c859afac11ba169a6
zg <wzgahaaa@gmail.com>
2026-07-07 18:30:54 +0800

    add github pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,8 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
 
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://www.hokkai2005.online',
+  site: 'https://wuzgng.github.io',
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
